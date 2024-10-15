a128f2602947dfa467443c7d1d1261b195264258
Easin Arafat <arafatmrx@gmail.com>
2024-10-15 11:16:03 +0600

    login error handle

--- tests/loginpagepractice.spec.js
+++ tests/loginpagepractice.spec.js
@@ -1,15 +1,25 @@
 const { test, expect } = require("@playwright/test");
 
 test("Browser Context Playwright test", async ({ page }) => {
-  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/", {
+    waitUntil: "domcontentloaded",
+  });
 
   console.log(await page.title());
 
   await page.locator("#username").fill("rahulshetty");
 
-  await page.locator('[type="password"]').fill("learning");
+  await page.locator("#password").fill("learning");
 
   await page.locator("#signInBtn").click();
 
-  await expect(page).toHaveTitle(/Login/);
+  const errorMessage = await page.locator("[style*='block']").textContent();
+
+  if (errorMessage) {
+    console.log(`Login error message: ${errorMessage}`);
+  } else {
+    console.log("Login successful.");
+  }
+
+  await expect(page.locator("[style*='block']")).not.toBeVisible();
 });
