7632187e4e345a98c2277609bfea1a2fbe3c7b21
Easin Arafat <arafatmrx@gmail.com>
2024-10-16 12:08:36 +0600

    more validations

---
+++ tests/moreValidaions.spec.js
@@ -0,0 +1,24 @@
+const { test, expect } = require("@playwright/test");
+
+test("popup validation", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+
+  await expect(page.locator("#displayed-text")).toBeVisible();
+  await page.locator("#hide-textbox").click();
+
+  await expect(page.locator("#displayed-text")).toBeHidden();
+  // await page.pause();
+  page.on("dialog", (dialog) => dialog.accept());
+
+  await page.locator("#confirmbtn").click();
+
+  const framesPage = await page.frameLocator("#courses-iframe");
+
+  // Inside the iframe
+  await framesPage.locator("li a[href*='lifetime-access']:visible").click();
+
+  const getTextH2 = await framesPage.locator(".content-side  h2").textContent();
+  const number = getTextH2.split(" ")[1];
+
+  console.log(number);
+});
